c095073136e767298957a2a5a1e5a0d34f17060b
Abdullah <abdullaharslan1473@gmail.com>
2024-09-12 13:24:02 +0300

    Initial Commit

---
+++ Apple-FrameWorks/Apple_FrameWorksApp.swift
@@ -0,0 +1,17 @@
+//
+//  Apple_FrameWorksApp.swift
+//  Apple-FrameWorks
+//
+//  Created by abdullah on 12.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct Apple_FrameWorksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
